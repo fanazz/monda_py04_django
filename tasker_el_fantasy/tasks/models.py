cbaa2cc6e9a3291a9ee189131835dd2b3ad8516c
fanazz <pskirma@gmail.com>
2024-02-12 22:02:40 +0200

    django_apps and_models_update

--- tasker_el_fantasy/tasks/models.py
+++ tasker_el_fantasy/tasks/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+
