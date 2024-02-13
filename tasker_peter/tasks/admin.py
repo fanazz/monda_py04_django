ff66e692e6bf31821844770dca913c121af80a2b
fanazz <pskirma@gmail.com>
2024-02-13 09:44:33 +0200

    Admin atnaujinimai1

--- tasker_peter/tasks/admin.py
+++ tasker_peter/tasks/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Project)
-admin.site.register(models.Task)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'owner']
+    list_display_links =['id','name']
+    list_filter = []
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name','is_done', 'deadline', 'project', 'owner', 'created_at']
+    list_filter = ['is_done', 'project', 'owner', 'deadline', 'created_at']
+
+admin.site.register(models.Project, ProjectAdmin)
+admin.site.register(models.Task, TaskAdmin)
