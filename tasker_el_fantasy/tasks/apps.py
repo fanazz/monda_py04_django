124d08cae7199d05034ecb06515df4c0b31d83b0
fanazz <pskirma@gmail.com>
2024-02-18 10:09:02 +0200

    istrintas projektas

--- tasker_el_fantasy/tasks/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-class TasksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
-
-    class Meta:
-        verbose_name = _('tasks')
--- tasker_el_fantasy/tasks/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-from django.urls import reverse
-from django.utils.translation import gettext as _
-
-
-
